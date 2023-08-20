f73976338f1fb8b19649aef5e98e7c050dbcb363
Sourav Kole <sourav.kole87@gmail.com>
2023-08-20 10:05:38 +0530

    changed Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,6 @@
 import React from "react";
 
 const Home = () => {
-  const mystyle = {
-    border: "0px",
-    overflow: "hidden",
-    width: "500px",
-    height: "500px",
-  };
   return (
     <div>
       <span>Welcome Hole..</span>
